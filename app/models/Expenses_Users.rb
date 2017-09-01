3e0168d6b70d4e22479a3853c4cdb32e791a1e02
Lawid <Ikachaden.Walid@hotmail.fr>
2017-09-01 12:10:52 +0200

    ...

--- app/models/Expenses_Users.rb
+++ app/models/Expenses_Users.rb
@@ -1,6 +1,6 @@
 class Expenses_users < ApplicationRecord
 
-    create_with :expense_user, id: false do |t|
+    create_table :expense_user, id: false do |t|
       t.belongs_to :expense, index: true
       t.belongs_to :user, index: true
     end
